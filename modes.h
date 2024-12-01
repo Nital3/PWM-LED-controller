4198cce181b5a61a4146d45f0150f7f911263d08
Nital <daniel.siwiaszczyk0@gmail.com>
2024-12-01 03:22:08 +0100

    Code optimalisation

--- modes.h
+++ modes.h
@@ -1,9 +1,11 @@
 #ifndef MODES_H
 #define MODES_H
 
-uint16_t fixed_brightness(int brightness_percentage, int wrap_point);
-uint16_t breathing(int timer_ms, double speed_multiplier, int wrap_point);
-uint16_t strobe(int timer_ms, int cycle_time, int wrap_point);
+const inline void switch_mode(int* current_mode_pointer, int* timer_pointer);
+const inline uint16_t fixed_brightness(int brightness_percentage, int wrap_point);
+const inline uint16_t breathing(int timer_ms, double speed_multiplier, int wrap_point);
+const inline uint16_t strobe(int timer_ms, int cycle_time, int wrap_point);
+
 
 
 #endif
